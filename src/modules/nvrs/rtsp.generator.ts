33dedcb51c5c491ee24483109a7f9b8adaa04a01
AgheraRaj <rajaghera2004@gmail.com>
2026-05-07 18:07:03 +0530

    update rtsp url for HIFOCUS

--- src/modules/nvrs/rtsp.generator.ts
+++ src/modules/nvrs/rtsp.generator.ts
@@ -7,17 +7,14 @@ interface NVRData {
 }
 
 export const generateRTSP = (nvr: NVRData, channel: number): string => {
-  const pad = channel.toString().padStart(2, "0");
-
   switch (nvr.type) {
-    case "HIKVISION":
     case "HIKVISION":
       // channel 2 sub-stream = 202, not 0202
       return `rtsp://${nvr.username}:${nvr.password}@${nvr.ip}:${nvr.rtspPort}/Streaming/Channels/${channel}02`;
 
     case "HIFOCUS":
-      // Hifocus format: /ch{channel}/0
-      return `rtsp://${nvr.username}:${nvr.password}@${nvr.ip}:${nvr.rtspPort}/ch${pad}/0`;
+      // User confirmed this format works in VLC: /chID=1&streamType=main
+      return `rtsp://${nvr.username}:${nvr.password}@${nvr.ip}:${nvr.rtspPort}/chID=${channel}&streamType=sub`;
 
     default:
       throw new Error(`Unsupported NVR type: ${nvr.type}`);
